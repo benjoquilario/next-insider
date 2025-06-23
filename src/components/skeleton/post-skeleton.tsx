2ae745ce379c295729039757f4d768b5637cb877
robertpangalinan <100414488+robertpangalinan@users.noreply.github.com>
2025-06-23 18:50:05 +0000

    fix: update postSkeleton and profileSkeleton components to use bg-accent for consistent styling

--- src/components/skeleton/post-skeleton.tsx
+++ src/components/skeleton/post-skeleton.tsx
@@ -3,24 +3,24 @@ const PostSkeleton = () => {
     <div className="mx-auto mb-4 w-full rounded-md p-3 shadow">
       <div className="flex animate-pulse flex-col space-y-5">
         <div className="flex items-center space-x-4">
-          <div className="size-11 rounded-full bg-primary/10 bg-opacity-80"></div>
+          <div className="bg-accent size-11 animate-pulse rounded-full"></div>
           <div className="flex-1 space-y-2 py-1">
-            <div className="h-6 w-36 rounded-full bg-primary/10"></div>
-            <div className="h-3 w-20 rounded-full bg-primary/10"></div>
+            <div className="bg-accent h-6 w-36 animate-pulse rounded-full"></div>
+            <div className="bg-accent h-3 w-20 animate-pulse rounded-full"></div>
           </div>
         </div>
         <div className="space-y-2">
-          <div className="h-3 w-full rounded bg-primary/10"></div>
-          <div className="h-3 w-full rounded bg-primary/10"></div>
-          <div className="h-3 w-full rounded bg-primary/10"></div>
+          <div className="bg-accent h-3 w-full animate-pulse rounded"></div>
+          <div className="bg-accent h-3 w-full animate-pulse rounded"></div>
+          <div className="bg-accent h-3 w-full animate-pulse rounded"></div>
         </div>
         <div>
-          <div className="h-40 w-full rounded bg-primary/10 md:h-56"></div>
+          <div className="bg-accent h-40 w-full animate-pulse rounded md:h-56"></div>
         </div>
         <div className="flex items-center justify-between gap-2">
-          <div className="h-[29px] w-full rounded bg-primary/10 md:h-[37px]"></div>
-          <div className="h-[29px] w-full rounded bg-primary/10 md:h-[37px]"></div>
-          <div className="h-[29px] w-full rounded bg-primary/10 md:h-[37px]"></div>
+          <div className="bg-accent h-[29px] w-full animate-pulse rounded md:h-[37px]"></div>
+          <div className="bg-accent h-[29px] w-full animate-pulse rounded md:h-[37px]"></div>
+          <div className="bg-accent h-[29px] w-full animate-pulse rounded md:h-[37px]"></div>
         </div>
       </div>
     </div>
